510a7f588d28eacd6ead9a3e40f9458abaa0fc90
Bui Khac Nam <buikhacnam11@gmail.com>
2020-10-15 11:22:52 +0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
     return (
         <Hero hero='aboutHero' >
                 <Banner style={{lineHeight: 1.5}} title="Bui Nam Travel" subtitle="Với sứ mệnh luôn nỗ lực hết mình để tối ưu hóa nhu cầu cho khách hàng, mang lại sự hài lòng và niềm tin trọn vẹn, Bui Nam Travel đã, đang và sẽ đa dạng hóa, nâng cao chất lượng dịch vụ đặt phòng khách sạn, vé máy bay cũng như cung cấp tour du lịch với mức giá ưu đãi nhất.">
-                    <Link to="/home" className="btn-primary">Trang Chủ</Link>
+                    <Link to="/" className="btn-primary">Trang Chủ</Link>
                 </Banner>
          </Hero>
     )
